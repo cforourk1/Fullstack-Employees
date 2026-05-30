d170a6dd76d84b9c1c015c3844cd8c64ee29cd5c
Crystal <cforourk@umich.edu>
2026-05-30 13:34:18 -0400

    password issue caused problems

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,21 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
+import { createEmployee } from "#db/queries/employees";
+
+
 
 await db.connect();
 await seedEmployees();
 await db.end();
 console.log("🌱 Database seeded.");
 
+// callin in faker to create the data for me!
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 10; i++) {
+    await createEmployee({
+      name: faker.person.fullName(),
+      birthday: faker.date.birthdate(),
+      salary: faker.number.int({ min: 30000, max: 150000 }),
+    });
+  }
 }
